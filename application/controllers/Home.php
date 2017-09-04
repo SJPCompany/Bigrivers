7ae6cad1019299bf87999b3eb020270de7c0b619
Stef Bakker <stefbakker@Live.nl>
2017-09-04 10:24:34 +0200

    Changes to header

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -20,6 +20,8 @@ class home extends CI_Controller {
 	 */
 	public function index()
 	{
+        $this->load->helper('url');
+        $this->load->view('templates/header');
 		$this->load->view('home_page');
 	}
 }
---
+++ application/views/templates/header.php
@@ -0,0 +1,7 @@
+<!-- Css/Fonts -->
+<link rel="icon" href="<?php echo base_url(); ?>images/favicon.ico" type="image/x-icon">
+
+<!-- Body -->
+<div class="header">
+    <img src="<?php echo base_url();?>images/header.png">
+</div>
