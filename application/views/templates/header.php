8606a83a6e64c311cf672b5309d68d0d95634d12
Stef Bakker <stefbakker@Live.nl>
2017-09-04 13:25:32 +0200

    Added logo, date and background.

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -12,5 +12,17 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 <body>
 <div class="header">
-    <img src="<?=base_url()?>img/header.png">
+    <div class="header-items">
+        <!-- <img class="header-beer" src="<?=base_url()?>img/beer.png"> -->
+        <!-- <img class="header-peanuts" src="<?=base_url()?>img/peanuts.png"> -->
+    </div>
+</div>
+<div class="header-coaster img-circle">
+    <div class="header-date-div">
+        <h3 class="header-date">14/15/16 juli!</h3>
+    </div>
+    <div class="pseudo-align"></div>
+    <a href="index/home">
+        <img class="header-logo" src="<?=base_url()?>img/logo.gif">
+    </a>
 </div>
