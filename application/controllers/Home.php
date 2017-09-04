c83c1d511ca8639d21f464916b533e99c88ed5d1
Stef Bakker <stefbakker@Live.nl>
2017-09-04 11:03:54 +0200

    Made changes in the config.php

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -20,8 +20,9 @@ class home extends CI_Controller {
 	 */
 	public function index()
 	{
-        $this->load->helper('url');
         $this->load->view('templates/header');
-		$this->load->view('home_page');
-	}
+        $this->load->view('home_page');
+        $this->load->view('templates/footer');
+
+    }
 }
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,7 +1,17 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Home Page - Big Rivers</title>
+</head>
 <!-- Css/Fonts -->
-<link rel="icon" href="<?php echo base_url(); ?>images/favicon.ico" type="image/x-icon">
+<link rel="icon" href="<?php echo base_url();?>images/favicon.ico" type="image/x-icon">
+<link rel="stylesheet" href="<?php echo base_url();?>css/style.css" type="text/css" />
 
-<!-- Body -->
+<body>
 <div class="header">
     <img src="<?php echo base_url();?>images/header.png">
+    yush
 </div>
