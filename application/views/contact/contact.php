bb131112dd31fc5c9a652d7f3d4b98b4c0e3a6a6
pieter <pieter1998@protonmail.com>
2017-09-05 10:18:31 +0200

    begin contact pagina met een nieuwe controller

---
+++ application/views/contact/contact.php
@@ -0,0 +1,4 @@
+<div class="container">
+<h1>Contact</h1>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2466.6382321084093!2d4.657104915951324!3d51.812770596150244!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c42ed1def4415f%3A0xf12d657582b4d26!2sSlikveld+1%2C+3311+VT+Dordrecht!5e0!3m2!1snl!2snl!4v1504598978692" width="100%" height="100%" frameborder="0" style="border:0" allowfullscreen></iframe>
+</div>
