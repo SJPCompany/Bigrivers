3cfc89f82a715b6a1c69c64a71078449e1d25b8a
pieter <pieter1998@protonmail.com>
2017-09-05 11:49:28 +0200

    contact page update

--- application/views/contact/contact.php
+++ application/views/contact/contact.php
@@ -1,4 +1,38 @@
 <div class="container">
 <h1>Contact</h1>
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2466.6382321084093!2d4.657104915951324!3d51.812770596150244!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c42ed1def4415f%3A0xf12d657582b4d26!2sSlikveld+1%2C+3311+VT+Dordrecht!5e0!3m2!1snl!2snl!4v1504598978692" width="100%" height="100%" frameborder="0" style="border:0" allowfullscreen></iframe>
+    <div class="map">
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2466.6382321084093!2d4.657104915951324!3d51.812770596150244!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c42ed1def4415f%3A0xf12d657582b4d26!2sSlikveld+1%2C+3311+VT+Dordrecht!5e0!3m2!1snl!2snl!4v1504598978692"
+            width="100%" height="300px" frameborder="0" style="border:0" allowfullscreen></iframe>
+    </div>
+
+    <div class="twitterfeed">
+        <a class="twitter-timeline" data-lang="nl" data-width="500" data-height="200" data-theme="light" data-link-color="#2B7BB9" href="https://twitter.com/BigRivers17">Tweets by BigRivers17</a> <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+    </div>
+
+    <div class="socialmedialinks">
+        <ul>
+            <li class="social"> <a href="http://www.facebook.com/bigriversdordrecht"><img src="<?=base_url()?>img/fb_icon.png"> </a></li>
+            <li class="social"> <a href="https://twitter.com/BigRivers17"><img src="<?=base_url()?>img/tw_icon.png"> </a></li>
+            <li class="social"> <a href="https://www.youtube.com/channel/UCJpdp-Ikda2SCWElN0cA8Og"><img src="<?=base_url()?>img/yt_icon.png"> </a></li>
+        </ul>
+    </div>
+
+    <div class="contactinformation">
+        <address>
+           <strong>Postadres</strong>
+            Stichting Big Rivers<br>
+            Postbus 1060<br>
+            3300 BB Dordrecht<br>
+            <br>
+            <br>
+         <strong>Bezoekadres</strong>
+            Festivalbureau Big Rivers<br>
+            Slikveld 1<br>
+            3311 VT Dordrecht<br>
+            <br>
+            <br>
+            <strong>Tel:</strong> (078-) 614 41 82<br>
+            <strong>E-mail:</strong> info@bigrivers.nl
+        </address>
+    </div>
 </div>
