da61233721d58259f77c5103551caf9e0d039935
Justin van de laar <justin555@live.nl>
2017-09-11 16:14:42 +0200

    Some style changes

--- application/views/errors/index.php
+++ application/views/errors/index.php
@@ -5,15 +5,15 @@
 </head>
 <body>
 
-<p>Something went wrong look below for more info</p>
+<h1>Something went wrong look below for more info</h1>
 <?php if(isset($_SESSION['error'])) {
     if ($_SESSION['error'] > 1) {
         foreach ($_SESSION['error'] as $item) {
-            echo $item;
+        ?> <p> <?php echo $item; ?> </p> <?php
             echo '<br /><br />';
         }
     } else {
-        echo $_SESSION['error'];
+        ?> <p> <?php echo $_SESSION['error']; ?> </p> <?php
     }
 } else {
         echo 'No errors orcured';
