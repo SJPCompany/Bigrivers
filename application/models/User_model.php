eba8f9a3d61a80c149674f5832b12f991db7c1fc
Justin van de laar <justin555@live.nl>
2017-09-11 12:13:27 +0200

    Added login page with redirect

---
+++ application/models/User_model.php
@@ -0,0 +1,13 @@
+<?php
+
+class user_model extends CI_Model {
+
+    public function __construct()
+    {
+        $this->load->database();
+    }
+
+    public function get_Userinfo() {
+
+    }
+}
