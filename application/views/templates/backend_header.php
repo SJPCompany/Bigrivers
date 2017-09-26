ff99fe605613fdbd0fe0a2ab81a1f8cac41abd27
Stef Bakker <stefbakker@live.nl>
2017-09-26 10:38:50 +0200

    no message

--- application/views/templates/backend_header.php
+++ application/views/templates/backend_header.php
@@ -189,7 +189,7 @@ desired effect
             <!-- Sidebar user panel (optional) -->
             <div class="user-panel">
                 <div class="pull-left image">
-                    <img src="http://www.digitspeak.com/wp-content/uploads/2016/02/4-1-300x295.png" class="img-circle" alt="User Image">
+                    <img src="#" class="img-circle" alt="User Image">
                 </div>
                 <div class="pull-left info">
                     <p><?= $_SESSION['username'] ?></p>
