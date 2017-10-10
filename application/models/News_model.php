28be8294a710bc200fae54e62d7b0d76f577dc4b
Unknown <pieter1998@protonmail.com>
2017-10-10 12:07:23 +0200

    news now shows up on the page/news and started newsedit page and functions

--- application/models/News_model.php
+++ application/models/News_model.php
@@ -22,4 +22,17 @@ class News_model extends CI_Model {
         return $this->db->insert('news', $data);
     }
 
+    public function get_news($slug = FALSE)
+    {
+        if ($slug === FALSE)
+        {
+            $query = $this->db->get('news');
+            return $query->result_array();
+        }
+
+        $query = $this->db->get_where('news', array('slug' => $slug));
+        return $query->row_array();
+    }
+
+
 }
