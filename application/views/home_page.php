e61d46051b134d7a6a931b27a7619a9ae3b1df8d
Justin van de laar <justin555@live.nl>
2017-11-13 09:52:57 +0100

    Little test

--- application/views/home_page.php
+++ application/views/home_page.php
@@ -2,7 +2,7 @@
     <div class="row ">
 
         <?php foreach($widgets as $widget) {
-        if($widget->active == '1') { ?>
+        if($widget->active == '2') { ?>
         <div class="Button col-xs-6 col-sm-4 col-md-3">
             <a href="/Home/Events/17" target="_self">
                 <img class="ButtonImg" src="https://upload.wikimedia.org/wikipedia/commons/c/c4/PM5544_with_non-PAL_signals.png">
