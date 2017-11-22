61fa95882984ce4ded5f712de5b7497caa4bf93d
ROMY\Romy96 <romybijkerk1996@gmail.com>
2017-11-22 11:07:10 +0100

    Organizing code

--- application/views/user/viewUsers.php
+++ application/views/user/viewUsers.php
@@ -42,57 +42,57 @@
                 echo '</ul></div>'; }?>
  <br>
  <div class="col-xs-12">
- <div class="box box-info">
-    <div class="box-header with-border">
-        <h3 class="box-title">Alle gebruikers</h3>
-        </div>
-        <!-- /.box-header -->
-        <div class="box-body">
-          <div id="example1_wrapper" class="dataTables_wrapper form-inline dt-bootstrap">
-            <div class="row">
-              <div class="col-sm-12">
-            <table id="sort" class="sort" role="grid" aria-describedby="example1_info">
-              <thead>
-              <tr role="row">
-                <th class="sorting" tabindex="0" aria-controls="example1" rowspan="1" colspan="1"
-                style="width: 120px;">Gebruikersnaam</th>
-                <th class="sorting" tabindex="0" aria-controls="example1" rowspan="1" colspan="1"
-                style="width: 120px;">Emailadres</th>
-                <th class="sorting" tabindex="0" aria-controls="example1" rowspan="1" colspan="1"
-                style="width: 120px;">Rol</th>
-                <th class="sorting" tabindex="0" aria-controls="example1" rowspan="1" colspan="1"
-                style="width: 120px;">Status</th>
-                <th class="sorting" tabindex="0" aria-controls="example1" rowspan="1" colspan="1"
-                style="width: 120px;">Bewerken</th>
-                <th class="sorting" tabindex="0" aria-controls="example1" rowspan="1" colspan="1"
-                style="width: 120px;">Verwijderen</th>
-              </tr>
-              </thead>
-              <tbody>
-            <?php foreach ($users as $user) { ?>
-              <tr>
-                <td><?= $user->username ?></td>
-                <td><?= $user->email ?></td>
-                <td><?= $user->name ?></td>
-                <?php if ($user->status == 0 || $user->status == '0') { ?>
-                    <td><span class="label label-success">Actief</span></td>
-                <?php } else { ?>
-                    <td><span class="label label-danger">Geblokt</span></td>
-                <?php } ?>
-                <td><a class="mid" href="<?= base_url('backend/user/editUser/'. $user->id) ?>"><i class="fa fa-pencil"></i></a></td>
-                <td><a href="<?= base_url('backend/user/delete/'. $user->id) ?>">Verwijder</a></td>
-              </tr>
-              <?php } ?>
-              </tbody>
-            </table>
+   <div class="box box-info">
+      <div class="box-header with-border">
+          <h3 class="box-title">Alle gebruikers</h3>
           </div>
-          <!-- /.table-responsive -->
-        </div>
-        <!-- /.box-footer -->
+          <!-- /.box-header -->
+          <div class="box-body">
+            <div id="example1_wrapper" class="dataTables_wrapper form-inline dt-bootstrap">
+              <div class="row">
+                <div class="col-sm-12">
+              <table id="sort" class="sort" role="grid" aria-describedby="example1_info">
+                <thead>
+                <tr role="row">
+                  <th class="sorting" tabindex="0" aria-controls="example1" rowspan="1" colspan="1"
+                  style="width: 120px;">Gebruikersnaam</th>
+                  <th class="sorting" tabindex="0" aria-controls="example1" rowspan="1" colspan="1"
+                  style="width: 120px;">Emailadres</th>
+                  <th class="sorting" tabindex="0" aria-controls="example1" rowspan="1" colspan="1"
+                  style="width: 120px;">Rol</th>
+                  <th class="sorting" tabindex="0" aria-controls="example1" rowspan="1" colspan="1"
+                  style="width: 120px;">Status</th>
+                  <th class="sorting" tabindex="0" aria-controls="example1" rowspan="1" colspan="1"
+                  style="width: 120px;">Bewerken</th>
+                  <th class="sorting" tabindex="0" aria-controls="example1" rowspan="1" colspan="1"
+                  style="width: 120px;">Verwijderen</th>
+                </tr>
+                </thead>
+                <tbody>
+              <?php foreach ($users as $user) { ?>
+                <tr>
+                  <td><?= $user->username ?></td>
+                  <td><?= $user->email ?></td>
+                  <td><?= $user->name ?></td>
+                  <?php if ($user->status == 0 || $user->status == '0') { ?>
+                      <td><span class="label label-success">Actief</span></td>
+                  <?php } else { ?>
+                      <td><span class="label label-danger">Geblokt</span></td>
+                  <?php } ?>
+                  <td><a class="mid" href="<?= base_url('backend/user/editUser/'. $user->id) ?>"><i class="fa fa-pencil"></i></a></td>
+                  <td><a href="<?= base_url('backend/user/delete/'. $user->id) ?>">Verwijder</a></td>
+                </tr>
+                <?php } ?>
+                </tbody>
+              </table>
+            </div>
+            <!-- /.table-responsive -->
+          </div>
+          <!-- /.box-footer -->
+      </div>
+    </div>
     </div>
   </div>
-</div>
-</div>
 </div>
  <script>
      new Tablesort(document.getElementById('sort'));
