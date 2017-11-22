f78110206338ed4284a64a24d747eda279fc17a3
ROMY\Romy96 <romybijkerk1996@gmail.com>
2017-11-22 13:14:18 +0100

    Slight change of style

--- application/views/podia/beheerpodia.php
+++ application/views/podia/beheerpodia.php
@@ -38,8 +38,8 @@
             echo '</ul></div>'; }?>
         <br>
     <div class="col-xs-12">
-        <div class="box">
-            <div class="box-header">
+        <div class="box box-info">
+                <div class="box-header with-border">
                 <h3 class="box-title">Beheer Podia</h3>
             </div>
             <!-- /.box-header -->
