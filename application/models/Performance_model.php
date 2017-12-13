bee02288c3a0a00b34f96a54e652cd5f8992f981
Justin van de laar <justin555@live.nl>
2017-12-13 10:15:21 +0100

    Een anonieme gebruiker kan op de frontpage alle evnementen/podium`s zien in de vorm van een blokken schema

--- application/models/Performance_model.php
+++ application/models/Performance_model.php
@@ -27,8 +27,10 @@ class Performance_model  extends CI_Model
 
     public function getPerformanceById($id) {
         $this->db->select('*');
-        $this->db->from('performance');
+        $this->db->from('performance pf');
         $this->db->where(array('event_days_id' => $id));
+        $this->db->join('podia p', 'pf.podium_id = p.id', 'left');
+        $this->db->order_by('pf.podium_id', 'ASC');
         $query = $this->db->get();
         if($query->num_rows() > 0) {
             return $query->result_array();
@@ -47,14 +49,14 @@ class Performance_model  extends CI_Model
         }
     }
 
-    public function getPerformanceInfo()
+    public function getPerformanceInfo($id)
     {
         $this->db->select('*');
         $this->db->from('performance_time pt');
         $this->db->join('performance p', 'pt.performance_id = p.id', 'left');
         $this->db->join('artists a', 'p.artist_id = a.id', 'left');
         $this->db->join('podia po', 'p.podium_id = po.id', 'left');
-        $this->db->order_by('p.podium_id', 'ASC');
+        $this->db->where(array('event_days_id' => $id));
 
         $query = $this->db->get();
 
